2dda6f86048065079272047d68b62e9c291e3bf8
Robin Rännar <robran@edu.umea.se>
2026-08-27 12:51:14 +0200

    Variabler-uppgift

---
+++ variabler.py
@@ -0,0 +1,16 @@
+husets_adress = "Strandgatan 5"
+husets_postkod = 78832
+husets_ålder = 2.5
+huset_är_bebott = True
+
+
+print(husets_adress)
+print(husets_postkod)
+print(husets_ålder)
+print(huset_är_bebott)
+
+print(type(husets_adress))
+print(type(husets_postkod))
+print(type(husets_ålder))
+print(type(huset_är_bebott))
+
