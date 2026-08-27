987b920d79ccdb0594d540445882c5541ed7ea80
Robin Rännar <robran@edu.umea.se>
2026-08-27 13:21:14 +0200

    Gissa talet + flera rundor av kalkylatorn

---
+++ gissa_talet.py
@@ -0,0 +1,28 @@
+import random
+
+target_number = random.randrange(0,10)
+
+MAX_TRIES = 3
+
+
+print("Vilket tal mellan 0 och 10 tänker datorn på?")
+
+tries = 0
+while tries < MAX_TRIES:
+    gissning = int(input("Vad gissar du? "))
+
+    if gissning == target_number:
+        print("Rätt!")
+        break
+    else:
+        print("Fel")
+        if gissning < target_number:
+            print("Gissa ett högre nummer")
+        else:
+            print("Gissa ett lägre nummer")
+
+        tries += 1
+
+if tries == MAX_TRIES:
+    print("Otur men du förlorade!")
+    print(f"Rätt svar var {target_number}")
--- kalkylator.py
+++ kalkylator.py
@@ -1,9 +1,13 @@
-tal_1 = float(input("Ange det första talet: ")) 
-tal_2 = float(input("Ange det andra talet: "))
+while True:
+    tal_1 = float(input("Ange det första talet: ")) 
+    tal_2 = float(input("Ange det andra talet: "))
 
-#Bytte från int till float för att hantera decimaltal
+    #Bytte från int till float för att hantera decimaltal
 
-print(f"Summa: {tal_1 + tal_2}")
-print(f"Differans: {tal_1 - tal_2}")
-print(f"Produkt: {tal_1 * tal_2}")
-print(f"Kvot: {tal_1/tal_2}")  
+    print(f"Summa: {tal_1 + tal_2}")
+    print(f"Differans: {tal_1 - tal_2}")
+    print(f"Produkt: {tal_1 * tal_2}")
+    print(f"Kvot: {tal_1/tal_2}")  
+
+    if input("Vill du fortsätta y/n? ") != "y":
+        break
